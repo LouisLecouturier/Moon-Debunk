7b97b505dbbf57b3a2da648566c3355c7650c9f4
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2022-02-27 15:30:02 +0100

    added accent-color

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="theme-color" content="#062743" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
