d3ab84c019d2129093a1865852ebe3f82d96bee5
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2022-04-04 19:57:38 +0200

    isloaded

--- components/Shared/Hero/Hero.jsx
+++ components/Shared/Hero/Hero.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./Hero.module.scss";
-
+import { useState } from "react";
 import Mouse from "../../../assets/icons/mouse.svg";
 
 const Hero = ({ title1, title2, text, page }) => {
+  const [loaded, setLoaded] = useState({ bg: false, moon: false });
+
   return (
     <section className={`scroll-snap ${styles.heroContainer}`}>
       <Image
@@ -11,6 +13,7 @@ const Hero = ({ title1, title2, text, page }) => {
         width={2268}
         height={4032}
         layout="fill"
+        onLoadingComplete={() => setLoaded({ ...loaded, bg: true })}
         src="/images/space.webp"
         alt="space"
       />
@@ -33,13 +36,14 @@ const Hero = ({ title1, title2, text, page }) => {
         </div>
 
         <div className={styles.heroShadow}></div>
-        {page && page === "index" && (
+        {page && loaded.bg && loaded.moon && page === "index" && (
           <div className={styles.moonContainer}>
             <div className={styles.moonShadow}></div>
             <Image
               className={styles.moon}
               src="/images/moon.webp"
               alt="moon"
+              onLoadingComplete={() => setLoaded({ ...loaded, moon: true })}
               width={500}
               height={500}
               layout="responsive"
